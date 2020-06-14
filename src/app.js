6bed189020ef1656e20c9f060f85dddf41c2b91d
Patrick Rizzardi <patrick.d.rizzardi@gmail.com>
2020-06-14 16:26:36 -0400

    Render individual expenses and connect store and component with redux

--- src/app.js
+++ src/app.js
@@ -1,25 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import AppRouter from './routers/AppRouter'
-import { addExpense } from './actions/expenses'
-import { setTextFilter } from './actions/filters'
-import getVisibleExpenses from './selectors/expenses'
-import configureStore from './store/configureStore'
-import 'normalize.css/normalize.css'
-import './styles/styles.scss'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
 
 const store = configureStore()
 
-store.dispatch(addExpense({ description: 'Water Bill' }))
+store.dispatch(addExpense({ description: 'Water Bill', amount: 4500 }))
 store.dispatch(addExpense({ description: 'Gas Bill' }))
+store.dispatch(setTextFilter('water'))
 
-store.dispatch(setTextFilter('bill'))
+setTimeout(() => {
+    store.dispatch(setTextFilter('bill'))
+}, 3000)
 
 const state = store.getState()
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
 
 console.log(visibleExpenses)
 
-store.getState()
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+)
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'))
+ReactDOM.render(jsx, document.getElementById('app'))
